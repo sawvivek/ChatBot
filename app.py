16e17bfdcfcb5c38be40a700f9c1533ae46badd9
Vivek saw <viveksaw144@gmail.com>
2026-09-05 18:55:35 +0530

    Day 1 project setup

---
+++ app.py
@@ -0,0 +1,6 @@
+print("================================")
+print("     AI FAQ ASSISTANT")
+print("================================")
+
+print("Project setup successful!")
+print("Python is working correctly.")
